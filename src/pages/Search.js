921ebcaafd61ffd0b2ea21967285f11eb45cf97d
Yasmin358 <amorin.yasmin378@gmail.com>
2022-07-06 14:48:28 -0300

    Resolução Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,41 @@ import React from 'react';
 import Header from '../components/Header.';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonDisabled: true,
+      input: '',
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const min = 2;
+    const { value } = target;
+    if (value.length >= min) {
+      this.setState({ buttonDisabled: false });
+    }
+  }
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.onInputChange }
+          name="input"
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ buttonDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
